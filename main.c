c74e9bfa453246378466473fb5b54c906d00432e
jenson200 <jenson@jensonbruins.nl>
2019-09-17 07:27:17 -0700

    Reset arm function

--- main.c
+++ main.c
@@ -1,15 +1,36 @@
 #include "ev3.h"
+
 int main()
 {
-    InitEV3();
-    int degrees = cm_to_degrees(5);
+	// Initialize EV3 lib
+	InitEV3();
 
-    
-    
-    FreeEV3();
-    return 0;
+	//Reset the arm and wait 500ms to make sure its reset
+	reset_arm();
+
+	//Drive forward till arm can reach crane right operator
+	//Move arm up
+	//Move arm down
+	//Drive backwards 
+	//Turn
+	//Drive forwards
+	//turn
+	//Drive forwards
+	//Operator other arm
+	//Do a dance
+	
+	int degrees = convert_cm(5);
+	FreeEV3();
+	return 0;
 }
 
-int cm_to_degrees(int cm) {
+void reset_arm() {
+	OnRevReg(OUT_A, 25);
+	Wait(MS_500);
+	Off(OUT_A);
+}
+
+int convert_cm(int cm) {
+	
 
 }
