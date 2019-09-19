4f4d464d227a3a913ab82f3b4cd8713f87c0c639
Roy Harsveld <royharsveld@gmail.com>
2019-09-19 11:49:03 +0200

    First iteration of bot

--- main.c
+++ main.c
@@ -6,14 +6,19 @@ int main()
 	InitEV3();
 	reset_arm();
 
-	RotateMotor(OUT_BC, 25, convert_cm(10));
+	RotateMotor(OUT_BC, 25, convert_cm(62));
+    // enough power to let the arm release
+     RotateMotor(OUT_A, -25, 80);
+    //RotateMotor(OUT_A, -25, 75);
+    
+    reset_arm();
 
 	FreeEV3();
 	return 0;
 }
 
 void reset_arm() {
-	OnRevReg(OUT_A, 100);
+	OnRevReg(OUT_A, 25);
 	Wait(MS_500);
 	Off(OUT_A);
 }
