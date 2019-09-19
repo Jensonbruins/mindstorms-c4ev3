b9b27dd9dbbdf1979314fefd90aea5c14ba480a5
jenson200 <jenson@jensonbruins.nl>
2019-09-19 02:15:53 -0700

    This stuff

--- main.c
+++ main.c
@@ -4,33 +4,25 @@ int main()
 {
 	// Initialize EV3 lib
 	InitEV3();
-
-	//Reset the arm and wait 500ms to make sure its reset
 	reset_arm();
 
-	//Drive forward till arm can reach crane right operator
-	//Move arm up
-	//Move arm down
-	//Drive backwards 
-	//Turn
-	//Drive forwards
-	//turn
-	//Drive forwards
-	//Operator other arm
-	//Do a dance
-	
-	int degrees = convert_cm(5);
+	RotateMotor(OUT_BC, 25, convert_cm(10));
+
 	FreeEV3();
 	return 0;
 }
 
 void reset_arm() {
-	OnRevReg(OUT_A, 25);
+	OnRevReg(OUT_A, 100);
 	Wait(MS_500);
 	Off(OUT_A);
 }
 
-int convert_cm(int cm) {
-	
+int convert_cm(int amount) {
+	const int degrees = 20;
+	int retval;
 
+	retval = amount * degrees;
+
+	return retval;
 }
