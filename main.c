194c724dab6b7c72de947df0871f398250ade491
jenson200 <jenson@jensonbruins.nl>
2019-09-24 03:37:57 -0700

    Cleaning up

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "ev3.h"
 
+/*
+ * function: main()
+ * Runs when program is init
+ */
 int main()
 {
 	// Initialize EV3 lib
@@ -7,52 +11,65 @@ int main()
 
 	// Reset arm to zero for program
 	reset_arm();
-	
-	// Rotatemotor (62cm) from base to crane
-	
-	// Start driving to first lever
-	RotateMotor(OUT_BC, 25, convert_cm(30));
-	turn_right();
-	RotateMotor(OUT_BC, 15, convert_cm(10));
-	turn_left();
-	RotateMotor(OUT_BC, 25, convert_cm(32));
-	// End driving to first lever
-	
-	// Start lever up
-	RotateMotor(OUT_A, -10, 72);
-	Wait(MS_100);
-	reset_arm();
-	// End lever up
-	
-	// Start driving to second lever
-	RotateMotor(OUT_BC, -25, convert_cm(15));
-	turn_left();
-	RotateMotor(OUT_BC, 15, convert_cm(10));
-	turn_right();
-	RotateMotor(OUT_BC, 25, convert_cm(15));
-	// End driving to second lever
-		
-	// Start lever up
-	RotateMotor(OUT_A, -25, 80);
-	Wait(MS_100);
-	reset_arm();
-	// End lever up
-	
-	RotateMotor(OUT_BC, -25, convert_cm(15));
-	Off(OUT_ALL);
 
-	dance();
+	crane_mission();
 
     	FreeEV3();
 	return 0;
 }
 
+/*
+ * function: crane_mission()
+ * Runs the crane mission from base
+ */
+void crane_mission() {
+	
+	// Start driving to first lever
+	RotateMotor(OUT_BC, 20, convert_cm(30));
+	turn_right();
+	RotateMotor(OUT_BC, 15, convert_cm(10));
+	turn_left();
+	RotateMotor(OUT_BC, 20, convert_cm(32));
+	
+	// Start lever up
+	RotateMotor(OUT_A, -10, 72);
+	Wait(MS_500);
+	reset_arm();
+	
+	// Start driving to second lever
+	RotateMotor(OUT_BC, -20, convert_cm(15));
+	turn_left();
+	RotateMotor(OUT_BC, 15, convert_cm(10));
+	turn_right();
+	RotateMotor(OUT_BC, 20, convert_cm(15));
+		
+	// Start lever up
+	RotateMotor(OUT_A, -20, 80);
+	Wait(MS_500);
+	reset_arm();
+
+	// Move away from obstacles
+	RotateMotor(OUT_BC, -20, convert_cm(15));
+	
+	// Shutdown motors
+	Off(OUT_ALL);
+}
+
+/*
+ * function: reset_arm()
+ * Resets the arm to its downright position
+ */
 void reset_arm() {
 	OnRevReg(OUT_A, 25);
 	Wait(MS_500);
 	Off(OUT_A);
 }
 
+
+/*
+ * Function: convert_cm(<amount of cm>)
+ * Converts a given amount of cm to wheel degrees
+ */
 int convert_cm(int amount) {
 	const int degrees = 20;
 	int retval;
@@ -62,6 +79,10 @@ int convert_cm(int amount) {
 	return retval;
 }
 
+/*
+ * function: turn_left()
+ * Will turn the bot 90deg left
+ */
 void turn_left() {
 	OnFwdReg(OUT_C, 25);
 	OnRevReg(OUT_B, 25);
@@ -70,6 +91,10 @@ void turn_left() {
 	Off(OUT_BC);
 }
 
+/*
+ * function: turn_right()
+ * Will turn the bot 90deg right
+ */
 void turn_right() {
 	OnFwdReg(OUT_B, 25);
 	OnRevReg(OUT_C, 25);
@@ -78,44 +103,30 @@ void turn_right() {
 	Off(OUT_BC);
 }
 
+/*
+ * function: dance()
+ * will make the bot party 
+ */
 void dance() {
-	OnFwdReg(OUT_B, 25);
-	OnRevReg(OUT_C, 25);
-	Wait(MS_200);
-	Off(OUT_BC);
 	
-	SetLedPattern(LED_GREEN);
-
-	RotateMotor(OUT_A, -25, 80);
+	// Turning
+	turn_left();
+	turn_right();
+	turn_right();
+	turn_left();
+	
+	// Arm
+	RotateMotor(OUT_A, -100, 70);
+	RotateMotor(OUT_A, -100, 35);
+	RotateMotor(OUT_A, 100, 35);
+	RotateMotor(OUT_A, -100, 35);
 	reset_arm();
 
-	SetLedPattern(LED_RED);
-	
-	PlaySound(SOUND_LOW_BEEP);
-
-	RotateMotor(OUT_A, -25, 80);
-	reset_arm();
-
-	SetLedPattern(LED_GREEN_PULSE);
-	Wait(MS_50);
-	SetLedPattern(LED_RED_PULSE);
-	
-	OnFwdReg(OUT_C, 25);
-	OnRevReg(OUT_B, 25);
-	Wait(MS_200);
-	Off(OUT_BC);
-
-	SetLedPattern(LED_ORANGE);
-
-	RotateMotor(OUT_A, -25, 80);
-	reset_arm();
-
-	PlaySound(SOUND_DOWN);
+	// Sound
 	PlaySound(SOUND_UP);
+	PlaySound(SOUND_CLICK);
 	PlaySound(SOUND_DOUBLE_BEEP);
-	PlaySound(FAST_UP);
-	
-	RotateMotor(OUT_A, -25, 80);
-	reset_arm();
-	
+	PlaySound(SOUND_FAST_UP);
+	StopSound();
 }
+
