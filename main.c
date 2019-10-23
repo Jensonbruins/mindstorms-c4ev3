24d3fe0743ddd9d2a69b005645ad9fb253a4b544
Jenson Bruins <jenson@jensonbruins.nl>
2019-10-23 21:41:40 +0200

    Comments comments and even more comments

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include "ev3.h"
 
 // Mission declaration
@@ -6,8 +5,10 @@ void crane_mission();
 void traffic_jam();
 void swing();
 void start_mission();
+
 // conversions
 int convert_cm(int amount);
+
 // Movements for the bot
 void reset_arm();
 void forward(int amount);
@@ -16,92 +17,113 @@ void turn_left();
 void turn_right();
 void turn_drive(int motor_a, int motor_b,int change_speed);
 
+/*
+ * Since: 1.0.0
+ * main()
+ * Everything will be executed from here
+ */
 int main() {
-	
+	// Initializing
 	InitEV3();
-
 	setAllSensorMode(GYRO_ANG, TOUCH_PRESS, NO_SEN, NO_SEN);
-
 	reset_arm();
 
+	// Missions
 	crane_mission();
-    start_mission();
-    traffic_jam();
-    start_mission();
-    swing();
+    	start_mission();
+    	traffic_jam();
+    	start_mission();
+    	swing();
 
-	// Disable everything before shutdown
+	// Terminating
 	Off(OUT_BC);
 	FreeEV3();
-	
-
-    return 0;
+    	return 0;
 }
 
 /*
+ * since: 1.0.0
  * crane_mission()
- * Runs the crane mission from base
+ * This will execute the crane mission from base when its called
  */
 void crane_mission() {
-	// Drive towards right arm
+	// Driving towards the first arm of the crane
 	forward(28);
 	turn_left();
 	forward(41);
 	
-	// Start right arm
+	// Moving the right arm of the crane
 	RotateMotor(OUT_A, -10, 88);
 	forward(8);
 	RotateMotor(OUT_A, 10, 19);
 	Wait(MS_500);
 	backward(10);
 	
-	// Reset arm for next mission
+	// Resetting the robotarm
 	reset_arm();
 
-	//Drive towards left arm
+	// Driving towards the seconds arm of the crane
 	turn_left();
 	forward(10);
 	turn_right();
 	forward(8);
 
-	// Start left arm
+	// Moving the right arm of the crane
 	RotateMotor(OUT_A, -10, 60);
-    Wait(MS_600);
+    	Wait(MS_600);
 	reset_arm();
 
-	// Drive away from left arm
+	// Driving back to the base
 	backward(60);
 }
 
 /*
+ * since: 1.1.0
  * traffic_jam()
- * This will start the traffic mission
+ * This will execute the crane mission from base when its called
  */
 void traffic_jam() {
+	// Driving towards the mission
 	forward(74);
 	turn_right();
+	
+	// Starting the lifting
 	forward(1);
 	RotateMotor(OUT_A, -100, 70);
 	Wait(MS_500);
-    forward(11);
-    Wait(MS_500);
-    turn_right();
-    forward(75);
-    reset_arm();
+    	forward(11);
+    	Wait(MS_500);
+    	turn_right();
+	
+	// Driving back to the base
+	forward(75);
+	reset_arm();
 }
 
-void swing(){
-    forward(20);
-    turn_right();
-    forward(150);
-    turn_right();
-    RotateMotor(OUT_A, -100, 70);
-    forward(15);
-    turn_left();
-    backward(155);
-    reset_arm();
-}
 /*
+ * since: 1.2.0
+ * swing()
+ * This will execute the swing mission from base when its called
+ */
+void swing(){
+	// Driving towards the mission
+	forward(20);
+	turn_right();
+	forward(150);
+	turn_right();
+
+	// Moving the swings arm
+	RotateMotor(OUT_A, -100, 70);
+	forward(15);
+	turn_left();
+
+	// Driving back to the base
+	backward(155);
+	reset_arm();
+}
+
+/*
+ * since: 1.0.1
  * reset_arm()
  * Resets the arm to its downright position
  */
@@ -111,60 +133,92 @@ void reset_arm() {
 	Off(OUT_A);
 }
 
+/*
+ * since: 1.2.1
+ * start_mission()
+ * Waits for the users input to execute the next mission
+ */
+
 void start_mission() {
-    do {
-        if (readSensor(IN_2) == 1) {
-            Wait(MS_800);
-        }
-     } while (readSensor(IN_2) == 0);
+	// Temporary fix should have a stop button implemented
+	// The if statement is useless?
+	do {
+		if (readSensor(IN_2) == 1) {
+			Wait(MS_800);
+		}
+	} while (readSensor(IN_2) == 0);
 }
 
+
 /*
-i * convert_cm(<amount of cm>)
+ * since: 1.0.2
+ * convert_cm(<amount in cm>)
  * Converts a given amount of cm to wheel degrees
  */
 int convert_cm(int amount) {
+	// This value is retrieved from the official legoleague site
 	const int degrees = 20;
 	int retval;
-
+	
 	retval = amount * degrees;
 
 	return retval;
 }
 
+/*
+ * since: 1.0.1
+ * forward(<amount in cm>)
+ * This will make the bot drive forwards with a consistent speed when called based on cm
+ */
 void forward(int amount) {
 	amount = convert_cm(amount);
 	RotateMotor(OUT_BC, 24, amount);
 }
 
+/*
+ * since: 1.0.1
+ * backward(<amount in cm>)
+ * This will make the bot drive backwards with a consistent speed when called based on cm
+ */
 void backward(int amount) {
 	amount = convert_cm(amount);
 	RotateMotor(OUT_BC, -15, amount);
 }
 
+/*
+ * <NOTE>
+ * Find a way to combine these two functions together
+ * </NOTE>
+ */
+
+/*
+ * since: 1.0.2
+ * turn_right()
+ * Will turn the robot right 90 degrees using the gyro sensor
+ */
 void turn_right() {
-	// Declaration for all the variables used
+	// Declaring alot of values
 	int gyro_value, gyro_offset, gyro_check_start_one, gyro_check_start_two, gyro_check_start_three, gyro_check_stop_one, gyro_check_stop_two, gyro_check_stop_three, gyro_check_final;
 	
-	// Create an offset value
+	// Offset value to fix the gyro sensor from doing weird stuff
 	gyro_offset = readSensor(IN_1);
 
-	// Filling stops with values
+	// Creating stop checks (This should be done differently)
+	// This is based pure on testing on the board
 	gyro_check_start_one = 10;
 	gyro_check_start_two = 20;
 	gyro_check_start_three = 30;
 	gyro_check_stop_one = 60;
 	gyro_check_stop_two = 70;
 	gyro_check_stop_three = 78;
-	gyro_check_final = 87; // 87 is decent
-	
-	// The loop that does stuff
+	gyro_check_final = 87;
+
 	while(1)
 	{
-		// Reading out the gyro value with offset
+		// Creating the gyro_value with offset subtracted
 		gyro_value = readSensor(IN_1) - gyro_offset;
 		
-		// Based on gyro value do some turn stuff
+		// Speeding up and down to make sure the bot doesn't overturn
 		if (gyro_value == 0) {
 			turn_drive(OUT_B,OUT_C,7);
 		} else if (gyro_value >= gyro_check_start_one && gyro_value < gyro_check_start_two) {
@@ -182,37 +236,41 @@ void turn_right() {
 			break;
 		}
 
-		// This is to prevent the stupid ev3 brick from crashing
+		// The ev3 brick cannot handle too much input so we timer it to make sure it doesn't crash
 		Wait(10);
 	}
-	// Calm the bot down after turning because it needs it
+	// Calming the bot down after turning because otherwise driving instantly will offset the bot
 	Wait(SEC_1);
 }
 
-
+/*
+ * since: 1.0.2
+ * turn_left()
+ * Will turn left 90 degrees using the gyro sensor
+ */
 void turn_left() {
-	// Declaration for all the variables used
+	// Declaring alot of values
 	int gyro_value, gyro_offset, gyro_check_start_one, gyro_check_start_two, gyro_check_start_three, gyro_check_stop_one, gyro_check_stop_two, gyro_check_stop_three, gyro_check_final;
 	
-	// Create an offset value
+	// Offset the gyro to prevent it from doing weird stuff
 	gyro_offset = readSensor(IN_1);
 
-	// Filling stops with values
+	// Creating stop checks()
+	// This is based purely on testing from the FLL board
 	gyro_check_start_one = -10;
 	gyro_check_start_two = -20;
 	gyro_check_start_three = -30;
 	gyro_check_stop_one = -60;
 	gyro_check_stop_two = -70;
 	gyro_check_stop_three = -80;
-	gyro_check_final = -86; //ben zeer tevreden wanneer -86
-	
-	// The loop that does stuff
+	gyro_check_final = -86;
+
 	while(1)
 	{
-		// Reading out the gyro value with offset
+		// Creation of gyro value with offset subtracted
 		gyro_value = readSensor(IN_1) - gyro_offset;
 		
-		// Based on gyro value do some turn stuff
+		// Speed up and down to make the perfect turn
 		if (gyro_value == 0) {
 			turn_drive(OUT_C,OUT_B,7);
 		} else if (gyro_value <= gyro_check_start_one && gyro_value > gyro_check_start_two) {
@@ -230,14 +288,18 @@ void turn_left() {
 			break;
 		}
 
-		// This is to prevent the stupid ev3 brick from crashing
+		// The ev3 brick cannot handle too much data so we timer it to prevent it from crashing
 		Wait(10);
 	}
-	// Calm the bot down after turning because it needs it
+	// Calming the bot down after turning because otherwise driving instantly will offset the bot
 	Wait(SEC_1);
 }
 
-
+/* 
+ * since: 1.0.3
+ * turn_drive(<motor forward>, <motor backward>, <speed of motors>)
+ * This function is made for the turn functions to prevent copypasta
+ */
 void turn_drive(int motor_a, int motor_b ,int change_speed) {
 	OnFwdReg(motor_a, change_speed);
 	OnRevReg(motor_b, change_speed);
